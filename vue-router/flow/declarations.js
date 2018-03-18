ef46e38325b574da6647639cc556a3c4fb918834
ustbhuangyi <280309453@qq.com>
2018-03-18 13:51:49 +0800

    add vue-router

---
+++ vue-router/flow/declarations.js
@@ -0,0 +1,102 @@
+declare var document: Document;
+
+declare class RouteRegExp extends RegExp {
+  keys: Array<{ name: string, optional: boolean }>;
+}
+
+declare type PathToRegexpOptions = {
+  sensitive?: boolean,
+  strict?: boolean,
+  end?: boolean
+}
+
+declare module 'path-to-regexp' {
+  declare var exports: {
+    (path: string, keys?: Array<?{ name: string }>, options?: PathToRegexpOptions): RouteRegExp;
+    compile: (path: string) => (params: Object) => string;
+  }
+}
+
+declare type Dictionary<T> = { [key: string]: T }
+
+declare type NavigationGuard = (
+  to: Route,
+  from: Route,
+  next: (to?: RawLocation | false | Function | void) => void
+) => any
+
+declare type AfterNavigationHook = (to: Route, from: Route) => any
+
+type Position = { x: number, y: number };
+type PositionResult = Position | { selector: string, offset?: Position } | void;
+
+declare type RouterOptions = {
+  routes?: Array<RouteConfig>;
+  mode?: string;
+  fallback?: boolean;
+  base?: string;
+  linkActiveClass?: string;
+  parseQuery?: (query: string) => Object;
+  stringifyQuery?: (query: Object) => string;
+  scrollBehavior?: (
+    to: Route,
+    from: Route,
+    savedPosition: ?Position
+  ) => PositionResult | Promise<PositionResult>;
+}
+
+declare type RedirectOption = RawLocation | ((to: Route) => RawLocation)
+
+declare type RouteConfig = {
+  path: string;
+  name?: string;
+  component?: any;
+  components?: Dictionary<any>;
+  redirect?: RedirectOption;
+  alias?: string | Array<string>;
+  children?: Array<RouteConfig>;
+  beforeEnter?: NavigationGuard;
+  meta?: any;
+  props?: boolean | Object | Function;
+  caseSensitive?: boolean;
+  pathToRegexpOptions?: PathToRegexpOptions;
+}
+
+declare type RouteRecord = {
+  path: string;
+  regex: RouteRegExp;
+  components: Dictionary<any>;
+  instances: Dictionary<any>;
+  name: ?string;
+  parent: ?RouteRecord;
+  redirect: ?RedirectOption;
+  matchAs: ?string;
+  beforeEnter: ?NavigationGuard;
+  meta: any;
+  props: boolean | Object | Function | Dictionary<boolean | Object | Function>;
+}
+
+declare type Location = {
+  _normalized?: boolean;
+  name?: string;
+  path?: string;
+  hash?: string;
+  query?: Dictionary<string>;
+  params?: Dictionary<string>;
+  append?: boolean;
+  replace?: boolean;
+}
+
+declare type RawLocation = string | Location
+
+declare type Route = {
+  path: string;
+  name: ?string;
+  hash: string;
+  query: Dictionary<string>;
+  params: Dictionary<string>;
+  fullPath: string;
+  matched: Array<RouteRecord>;
+  redirectedFrom?: string;
+  meta?: any;
+}
